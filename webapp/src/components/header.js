4a0c5d564c26ef97aea832b2daeaacbadff880a2
Nando-Freitas <nando.freitas01@gmail.com>
2023-03-26 13:21:14 -0300

    0001 - refactor: removing unused code

--- webapp/src/components/header.js
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import HeaderStyled from './header.style.js'
-
-export default function Header() {
-    return (
-        <HeaderStyled>oi</HeaderStyled>
-    )
-};
--- webapp/src/components/header.style.js
+++
@@ -1,7 +0,0 @@
-import styled from '@emotion/styled';
-
-const HeaderStyled = styled.h1`
-  color: hotpink;
-`;
-
-export default HeaderStyled;
