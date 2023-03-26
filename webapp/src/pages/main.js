c3a6f4a2d721bf05ee19c1ec3c870296c335feb6
Nando-Freitas <nando.freitas01@gmail.com>
2023-03-26 20:56:36 -0300

    0001 feat: add menu component on main

--- webapp/src/pages/main.js
+++ webapp/src/pages/main.js
@@ -1,15 +1,21 @@
 import * as React from 'react';
 import ShowItems from './showItems';
 import Search from '../components/search';
+import Menu from '../components/menu';
+
 import { useState } from 'react';
 
 export default function Main() {
     const [search, setSearch] = useState('');
+    const [menuItemSelected, setMenuItemSelected] = useState('');
+
+    const menuItems = ['Characters', 'Episodes', 'Locations'];
 
     return(
         <>
+            <Menu menuItems={menuItems} setMenuItemSelected={setMenuItemSelected}/>
             <Search search={search} setSearch={setSearch}/>
-            <ShowItems search={search}/>
+            <ShowItems search={search} menuItemSelected={menuItemSelected}/>
         </>
     )
 }
