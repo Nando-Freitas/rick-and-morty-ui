a608354ec385b8e5a2fc66ff1b60d2f9276ed4db
Nando-Freitas <nando.freitas01@gmail.com>
2023-03-26 18:14:13 -0300

    001 feat: add search table filtered

--- webapp/src/components/table.js
+++ webapp/src/components/table.js
@@ -1,23 +1,114 @@
 import * as React from 'react';
 
 export default function Table(props) {
-    const { items } = props;
+    const { items, search } = props;
+    console.log(items.data.results)
     
-    return (
-        <table>
-            <tbody>
-                {items.data.results.map(item => {
-                    return (
-                        <tr key={item.id}>
-                            <td><img alt={item.name} src={item.image} /></td>
-                            <td>{item.name}</td>
-                            <td>{item.status}</td>
-                            <td>{item.species}</td>
-                            <td>{item.type}</td>
-                        </tr>
-                    )
-                })};
-            </tbody>
-        </table>
-    )
+    if(items.areCharacters === true){
+        return (
+            <table>
+                <thead>
+                    <tr>
+                        <td></td>
+                        <td>Name</td>
+                        <td>Status</td>
+                        <td>Specie</td>
+                        <td>Type</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {search === '' ? items.data.results.map(item => {
+                        return (
+                            <tr key={item.id}>
+                                <td><img alt={item.name} src={item.image} /></td>
+                                <td>{item.name}</td>
+                                <td>{item.status}</td>
+                                <td>{item.species}</td>
+                                <td>{item.type}</td>
+                            </tr>
+                        )
+                    }) : items.data.results.filter(item => {return item.name.includes(search)}).map(item => {
+                        return (
+                            <tr key={item.id}>
+                                <td><img alt={item.name} src={item.image} /></td>
+                                <td>{item.name}</td>
+                                <td>{item.status}</td>
+                                <td>{item.species}</td>
+                                <td>{item.type}</td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+        )
+    }
+
+    if(items.areLocations === true) {
+        return (
+            <table>
+                <thead>
+                    <tr>
+                        <td>Name</td>
+                        <td>Dimension</td>
+                        <td>Type</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {search === '' ? items.data.results.map(item => {
+                        return (
+                            <tr key={item.id}>
+                                <td>{item.name}</td>
+                                <td>{item.dimension}</td>
+                                <td>{item.type}</td>
+                            </tr>
+                        )
+                    }) : items.data.results.filter(item => {return item.name.includes(search)}).map(item => {
+                        return (
+                            <tr key={item.id}>
+                                <td>{item.name}</td>
+                                <td>{item.dimension}</td>
+                                <td>{item.type}</td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+        )
+    }
+    
+    if(items.areEpisodes === true) {
+        return (
+            <table>
+                <thead>
+                    <tr>
+                        <td>Name</td>
+                        <td>Episode</td>
+                        <td>Created At</td>
+                        <td>Air Date</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {search === '' ? items.data.results.map(item => {
+                        return (
+                            <tr key={item.id}>
+                                <td>{item.name}</td>
+                                <td>{item.episode}</td>
+                                <td>{item.created}</td>
+                                <td>{item.air_date}</td>
+                            </tr>
+                        )
+                    }) : items.data.results.filter(item => item.name.includes(search) ).map(item => {
+                        return (
+                            <tr key={item.id}>
+                                <td>{item.name}</td>
+                                <td>{item.episode}</td>
+                                <td>{item.created}</td>
+                                <td>{item.air_date}</td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+        )
+    }
 }
