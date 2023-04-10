5bcd9e7d446ed4b5016eb8c919729c78b527fd54
Nando-Freitas <nando.freitas01@gmail.com>
2023-04-10 00:38:08 -0300

    001 style (modal)

---
+++ webapp/src/components/moreInfoModal.style.js
@@ -0,0 +1,26 @@
+const moreInfoModalContainer = {
+    position: 'fixed',
+    top: '0px',
+    backgroundColor: '#97ce4c',
+    padding: '10px 40px 40px 40px',
+    borderRadius: '10px',
+    transform: 'translate(25%, 100%)',
+    width: '500px',
+    transition: 'visibility 3s'
+}
+
+const closeButtonStyled = {
+    backgroundColor: '#97ce4c',
+    border: 'none',
+    cursor: 'pointer'
+}
+
+const imgModalStyled = {
+    borderRadius: '50%',
+    width: '100px',
+    height: '100px',
+    marginRight: '5%',
+    marginTop: '3%'
+}
+
+export {moreInfoModalContainer, closeButtonStyled, imgModalStyled};
